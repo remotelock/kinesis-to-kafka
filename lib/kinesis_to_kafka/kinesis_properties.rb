eafbe6cb5aa8dce600a0629f6c054f877b6e7386
Rafael Sales <rafaelcds@gmail.com>
2021-05-03 02:10:59 -0300

    Use config folder for generated properties file

--- lib/kinesis_to_kafka/kinesis_properties.rb
+++ lib/kinesis_to_kafka/kinesis_properties.rb
@@ -3,7 +3,7 @@ require 'erb'
 module KinesisToKafka
   module KinesisProperties
     TEMPLATE_FILE = KinesisToKafka.root.join('config', 'kinesis_consumer.properties.erb').to_s
-    OUTPUT_FILE = KinesisToKafka.root.join('tmp', 'kinesis_consumer.properties').to_s
+    OUTPUT_FILE = KinesisToKafka.root.join('config', 'kinesis_consumer.properties').to_s
 
     def self.generate_file
       erb = ERB.new(File.read(TEMPLATE_FILE))
